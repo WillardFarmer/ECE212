7d2cd66797f074a46fee8dab4ad11d68d43f6931
Willard Farmer <32047734+WillardFarmer@users.noreply.github.com>
2018-03-27 20:16:46 -0600

    Added comments

--- Lab3/Lab3c.s
+++ Lab3/Lab3c.s
@@ -9,103 +9,104 @@
 
 /******************************************************************/
 /* General Information ********************************************/
-/* File Name: Lab3c.s **************************************/
-/* Names of Students: _________________ and ____________________ **/
-/* Date: _________________                                       **/
+/* File Name: Lab3a.s *********************************************/
+/* Names of Students: Willard Farmer and Chris Pontikes          **/
+/* Date: March 22, 2018                                          **/
 /* General Description:                                          **/
 /*                                                               **/
 /******************************************************************/
 Display:
 /*Write your program here******************************************/
 
-move.l %d7, -(%sp)
-move.l #0x2300000, %a2
-move.l #0x2310000, %a3
+move.l %d7, -(%sp)        /*Only %d7 is being used, so only it will be backed up*/
+move.l #0x2300000, %a2    /*The test program did not pass correct values for the address*/
+move.l #0x2310000, %a3    /*registers so they've been hardcoded into the subroutine.    */
 
-jsr cr
-jsr cr
+jsr cr                  /*                  */
+jsr cr                  /*Carriage Return x2*/
 
-pea NumEntries
-jsr iprintf
-move.l 24(%sp), (%sp)
-jsr value
-addq.l #4, %sp
-jsr cr
+pea NumEntries          /*"The number of entries was "*/
+jsr iprintf             /*Print*/
+move.l 24(%sp), (%sp)   /*Move Number of Entries into the stack*/
+jsr value               /*Print Number of Entries*/
+addq.l #4, %sp          /*Clean Stack*/
+jsr cr                  /*Carriage Return*/
 
 
+/************/
 /*Entry list*/
-move.l 20(%sp), %d7		/*Counter*/
-pea NumEntList
-jsr iprintf
-addq.l #4, %sp
+
+move.l 20(%sp), %d7		  /*Counter for number of entries*/
+pea NumEntList          /*"The inputed numbers are:"*/
+jsr iprintf             /*Print*/
+addq.l #4, %sp          /*Clean Stack*/
 
 InputList:
-jsr cr
-move.l (%a2)+, -(%sp)
-jsr value
-addq.l #4, %sp
-subq.l #1, %d7
-tst.l %d7
-bne InputList
+jsr cr                  /*Carriage Return*/
+move.l (%a2)+, -(%sp)   /*Move Entry onthe the stack*/
+jsr value               /*Print the entry*/
+addq.l #4, %sp          /*Clean the stack*/
+subq.l #1, %d7          /*Decrement the counter*/
+tst.l %d7               /*Test if the counter has reached zero*/
+bne InputList           /*Loop for the whole array*/
 
-jsr cr
 
-pea MinNum
-jsr iprintf
-move.l (%a3)+, (%sp)
-jsr value
-addq.l #4, %sp
-jsr cr
+jsr cr                  /*Carriage Return*/
+pea MinNum              /*"Min Number =: "*/
+jsr iprintf             /*Print*/
+move.l (%a3)+, (%sp)    /*Move Min Number onto the stack*/
+jsr value               /*Print min num*/
+addq.l #4, %sp          /*Clean Stack*/
 
-pea MaxNum
-jsr iprintf
-move.l (%a3)+, (%sp)
-jsr value
-addq.l #4, %sp
-jsr cr
+jsr cr                  /*Carriage Return*/
+pea MaxNum              /*"Max Number =: "*/
+jsr iprintf             /*Print*/
+move.l (%a3)+, (%sp)    /*Move Max Number onto the stack*/
+jsr value               /*Print max num*/
+addq.l #4, %sp          /*Clean Stack*/
 
-pea MeanNum
-jsr iprintf
-move.l (%a3)+, (%sp)
-jsr value
-addq.l #4, %sp
-jsr cr
+jsr cr                  /*Carriage Return*/
+pea MeanNum             /*"Mean Number =: "*/
+jsr iprintf             /*Print*/
+move.l (%a3)+, (%sp)    /*Move Mean Number onto the stack*/
+jsr value               /*Print max num*/
+addq.l #4, %sp          /*Clean Stack*/
 
-pea NumDiv1
-jsr iprintf
-move.l 28(%sp), (%sp)
-jsr value
-pea NumDiv2
-jsr iprintf
-move.l 24(%sp), (%sp)
-jsr value
-addq.l #8, %sp
-jsr cr
+jsr cr                  /*Carriage Return*/
+pea NumDiv1             /*"There are "*/
+jsr iprintf             /*Print*/
+move.l 28(%sp), (%sp)   /*Move number of divisors to the stack*/
+jsr value               /*Print number of divisors*/
+pea NumDiv2             /* number(s) divisible by */
+jsr iprintf             /*Print*/
+move.l 24(%sp), (%sp)   /*Move divisor to the stack*/
+jsr value               /*Print divisor*/
+addq.l #8, %sp          /*Clean the stack*/
+jsr cr                  /*Carriage Return*/
 
 
 /*Divisor list*/
-move.l 24(%sp), %d7		/*Counter*/
-pea NumDivList
-jsr iprintf
-addq.l #4, %sp
+move.l 24(%sp), %d7		  /*Initialize the divisor counter*/
+pea NumDivList          /*"The divisible numbers are:"*/
+jsr iprintf             /*Print*/
+addq.l #4, %sp          /*Clean the stack*/
 
 DivisorList:
-jsr cr
-move.l (%a3)+, -(%sp)
-jsr value
-addq.l #4, %sp
-subq.l #1, %d7
-tst.l %d7
-bne DivisorList
+jsr cr                  /*Carriage Return*/
+move.l (%a3)+, -(%sp)   /*Push divisable number to the stack*/
+jsr value               /*Print divisable number*/
+addq.l #4, %sp          /*Clean the stack*/
+subq.l #1, %d7          /*Decrement the counter*/
+tst.l %d7               /*Test if the counter has reached zero*/
+bne DivisorList         /*Loop for the whole array*/
+jsr cr                  /*Carriage Return*/
 
-jsr cr
+pea Ended               /*"Program ended"*/
+jsr iprintf             /*Print*/
+addq.l #4, %sp          /*Clean the stack*/
+jsr cr                  /*Carriage Return*/
 
-pea Ended
-jsr iprintf
-addq.l #4, %sp
-jsr cr
-
-move.l (%sp)+, %d7
+move.l (%sp)+, %d7      /*Reload %d7*/
 
 
 rts 
