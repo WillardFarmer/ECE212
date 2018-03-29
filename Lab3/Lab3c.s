5dd75870c514d83225b14d275b40cc30909944fa
Willard Farmer <32047734+WillardFarmer@users.noreply.github.com>
2018-03-28 18:32:39 -0600

    Update Lab3c.s

--- Lab3/Lab3c.s
+++ Lab3/Lab3c.s
@@ -18,16 +18,17 @@
 Display:
 /*Write your program here******************************************/
 
-move.l %d7, -(%sp)        /*Only %d7 is being used, so only it will be backed up*/
-move.l #0x2300000, %a2    /*The test program did not pass correct values for the address*/
-move.l #0x2310000, %a3    /*registers so they've been hardcoded into the subroutine.    */
+subq.l #12, %sp
+movem.l %d7/%a2-%a3, (%sp)  /*Only %d7 is being used, so only it will be backed up*/
+move.l #0x2300000, %a2      /*The test program did not pass correct values for the address*/
+move.l #0x2310000, %a3      /*registers so they've been hardcoded into the subroutine.    */
 
 jsr cr                  /*                  */
 jsr cr                  /*Carriage Return x2*/
 
 pea NumEntries          /*"The number of entries was "*/
 jsr iprintf             /*Print*/
-move.l 24(%sp), (%sp)   /*Move Number of Entries into the stack*/
+move.l 32(%sp), (%sp)   /*Move Number of Entries into the stack*/
 jsr value               /*Print Number of Entries*/
 addq.l #4, %sp          /*Clean Stack*/
 jsr cr                  /*Carriage Return*/
@@ -36,7 +37,7 @@ jsr cr                  /*Carriage Return*/
 /************/
 /*Entry list*/
 
-move.l 20(%sp), %d7     /*Counter for number of entries*/
+move.l 28(%sp), %d7     /*Counter for number of entries*/
 pea NumEntList          /*"The inputed numbers are:"*/
 jsr iprintf             /*Print*/
 addq.l #4, %sp          /*Clean Stack*/
@@ -75,18 +76,18 @@ addq.l #4, %sp          /*Clean Stack*/
 jsr cr                  /*Carriage Return*/
 pea NumDiv1             /*"There are "*/
 jsr iprintf             /*Print*/
-move.l 28(%sp), (%sp)   /*Move number of divisors to the stack*/
+move.l 36(%sp), (%sp)   /*Move number of divisors to the stack*/
 jsr value               /*Print number of divisors*/
 pea NumDiv2             /* number(s) divisible by */
 jsr iprintf             /*Print*/
-move.l 24(%sp), (%sp)   /*Move divisor to the stack*/
+move.l 32(%sp), (%sp)   /*Move divisor to the stack*/
 jsr value               /*Print divisor*/
 addq.l #8, %sp          /*Clean the stack*/
 jsr cr                  /*Carriage Return*/
 
 
 /*Divisor list*/
-move.l 24(%sp), %d7     /*Initialize the divisor counter*/
+move.l 32(%sp), %d7     /*Initialize the divisor counter*/
 pea NumDivList          /*"The divisible numbers are:"*/
 jsr iprintf             /*Print*/
 addq.l #4, %sp          /*Clean the stack*/
@@ -106,8 +107,8 @@ jsr iprintf             /*Print*/
 addq.l #4, %sp          /*Clean the stack*/
 jsr cr                  /*Carriage Return*/
 
-move.l (%sp)+, %d7      /*Reload %d7*/
-
+movem.l (%sp), %d7/%a2-%a3      /*Reload registers*/
+addq.l #12, %sp
 
 rts 
 
